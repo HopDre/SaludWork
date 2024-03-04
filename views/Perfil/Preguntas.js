047a49a7fe385b685be5a58a013017a05ea007ae
HopDre <113332878+HopDre@users.noreply.github.com>
2024-03-04 16:52:49 -0300

    Add files via upload

--- views/Perfil/Preguntas.js
+++ views/Perfil/Preguntas.js
@@ -11,13 +11,14 @@ import { useNavigation } from "@react-navigation/native";
 
 const Preguntas = () => {
   const initialTexts = [
-    "Lorem ipsum dolor sit amet consectetur adipiscing elit sociis, duis malesuada velit praesent ridiculus ullamcorper quam himenaeos per, fermentum litora porta gravida sapien eleifend montes. Senectus vel sollicitudin viverra malesuada cras semper curabitur accumsan montes potenti elementum",
-    "Lorem ipsum dolor sit amet consectetur adipiscing elit sociis, duis malesuada velit praesent ridiculus ullamcorper quam himenaeos per, fermentum litora porta gravida sapien eleifend montes. Senectus vel sollicitudin viverra malesuada cras semper curabitur accumsan montes potenti elementum",
-    "Lorem ipsum dolor sit amet consectetur adipiscing elit sociis, duis malesuada velit praesent ridiculus ullamcorper quam himenaeos per, fermentum litora porta gravida sapien eleifend montes. Senectus vel sollicitudin viverra malesuada cras semper curabitur accumsan montes potenti elementum",
-    "Lorem ipsum dolor sit amet consectetur adipiscing elit sociis, duis malesuada velit praesent ridiculus ullamcorper quam himenaeos per, fermentum litora porta gravida sapien eleifend montes. Senectus vel sollicitudin viverra malesuada cras semper curabitur accumsan montes potenti elementum",
-    "Lorem ipsum dolor sit amet consectetur adipiscing elit sociis, duis malesuada velit praesent ridiculus ullamcorper quam himenaeos per, fermentum litora porta gravida sapien eleifend montes. Senectus vel sollicitudin viverra malesuada cras semper curabitur accumsan montes potenti elementum",
-    "Lorem ipsum dolor sit amet consectetur adipiscing elit sociis, duis malesuada velit praesent ridiculus ullamcorper quam himenaeos per, fermentum litora porta gravida sapien eleifend montes. Senectus vel sollicitudin viverra malesuada cras semper curabitur accumsan montes potenti elementum",
-    "Lorem ipsum dolor sit amet consectetur adipiscing elit sociis, duis malesuada velit praesent ridiculus ullamcorper quam himenaeos per, fermentum litora porta gravida sapien eleifend montes. Senectus vel sollicitudin viverra malesuada cras semper curabitur accumsan montes potenti elementum",
+    "¿Cómo puedo registrarme como profesional de la salud en la aplicación?\n\nPara registrarte como profesional de la salud en nuestra aplicación, simplemente sigue los pasos de registro en la sección designada para proveedores. Deberás proporcionar información sobre tu licencia médica y otros detalles relevantes para verificar tu identidad y credenciales.",
+    "¿Qué tipo de servicios médicos puedo ofrecer a través de la aplicación?\n\nNuestra aplicación permite a los profesionales de la salud ofrecer una variedad de servicios, que van desde consultas generales hasta servicios especializados según tu área de expertise. Puedes especificar los servicios que ofreces durante el proceso de registro.",
+    "¿Cómo funciona el proceso de programación de citas desde mi perspectiva como proveedor de salud?\n\nUna vez que estés registrado como proveedor en la aplicación, recibirás notificaciones sobre las solicitudes de citas de los usuarios. Puedes revisar estas solicitudes y confirmar las citas según tu disponibilidad.",
+    "¿Cómo garantiza la aplicación la privacidad y seguridad de la información médica que comparto?\n\nLa aplicación utiliza medidas de seguridad avanzadas para proteger la información médica compartida entre los usuarios y los proveedores de salud. Además, cumplimos con las regulaciones de privacidad de datos para garantizar la confidencialidad de la información médica.",
+    "¿Cuál es el proceso de pago por los servicios médicos prestados a través de la aplicación?\n\nLos pagos por los servicios médicos se gestionan de manera segura a través de la aplicación. Una vez que completes una consulta o servicio, recibirás el pago según los términos y condiciones acordados previamente.",
+    "¿Qué debo hacer en caso de emergencia médica durante una consulta a través de la aplicación?\n\nSi un usuario experimenta una emergencia médica durante una consulta, asegúrate de proporcionar orientación inicial y, si es necesario, recomienda buscar atención médica presencial de inmediato. La aplicación también proporciona recursos de emergencia para ayudar a los usuarios en situaciones críticas.",
+    "¿Puedo acceder a la información médica de mis pacientes a través de la aplicación?\n\nSí, como proveedor de salud, tendrás acceso a la información médica relevante de tus pacientes a través de la aplicación. Esto te permite proporcionar un mejor cuidado al tener un historial médico completo disponible durante las consultas.",
+    "¿Qué debo hacer si tengo problemas técnicos durante una consulta a través de la aplicación?\n\nSi experimentas problemas técnicos durante una consulta, comunícate con nuestro equipo de soporte técnico para obtener asistencia inmediata. También puedes ofrecer a los usuarios la opción de reprogramar la consulta si es necesario resolver el problema técnico.",
   ];
 
   const [visibleTextIndex, setVisibleTextIndex] = useState(null);
@@ -58,7 +59,7 @@ const Preguntas = () => {
               onPress={() => toggleTextVisibility(index)}
               style={styles.button}
             >
-              <Text style={styles.buttonText}>Pregunta {index + 1}</Text>
+              <Text style={styles.buttonText}>{index + 1}</Text>
             </TouchableOpacity>
 
             {visibleTextIndex === index && (
@@ -97,6 +98,7 @@ const styles = StyleSheet.create({
   contenedor: {
     flex: 1,
     width: "100%",
+    marginBottom:45,
   },
   button: {
     backgroundColor: "#0074BD",
